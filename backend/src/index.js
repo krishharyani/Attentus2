4fc4876b5bc02c24363f8027a887bce313c3c3b7
ylabidi11 <youssef.labidi35@gmail.com>
2025-08-03 00:03:14 -0400

    pu

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,4 +29,6 @@ app.use('/api/notes', noteRoutes);
 app.use('/api/chats', chatRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Backend running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Backend running on port ${PORT}`);
+});
